4585bc1b315e33d22e02b759d70c428dcd516b4d
Patrick Gustafson <patrickgus@charter.net>
2019-12-10 08:42:28 -0800

    fix index files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import "typeface-roboto";
 import "./index.css";
+import App from "./App/App";
 
 ReactDOM.render(
   <BrowserRouter>
