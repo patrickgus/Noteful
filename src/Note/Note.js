d46a42344f128a9390ef4c486845379b2a2159b0
Patrick Gustafson <patrickgus@charter.net>
2019-12-10 09:14:14 -0800

    add date-fns

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { format } from "date-fns";
 import "./Note.css";
 
 export default function Note(props) {
@@ -11,6 +12,12 @@ export default function Note(props) {
       <button className="Note__delete" type="button">
         remove
       </button>
+      <div className="Note__dates">
+        <div className="Note__dates-modified">
+          Modified{" "}
+          <span className="Date">{format(props.modified, "Do MMM YYYY")}</span>
+        </div>
+      </div>
     </div>
   );
 }
